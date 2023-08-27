231f82994322a0408f8d24f43bc4858fe08f2cf5
Nabin Malla <nabin.malla@patancollege.edu.np>
2023-08-27 09:09:06 +0545

    route

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+// const router = require("express").Router();
+
+// router.get("/", (req, res) => {
+//   res.send("Hello World");
+// });
+
+// router.get("/:name/:roll", (req, res) => {
+//   console.log(req.query) // query
+//   const(name,roll)= req.params  //params
+//   res.send(`Hello${name}.you are from ${adress} and your roll is ${roll}`);
+
+// });
