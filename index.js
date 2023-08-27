9952bd1239ee9016aa13428f286bd071078b16d5
Nabin Malla <nabin.malla@patancollege.edu.np>
2023-08-27 23:50:43 +0545

    my portfolio

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const router = require("./routes/index"); // Adjust the path to your router file
 // Use the router for specific routes
 app.use("/", router); // Replace "your-base-route" with the desired base route
 
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.use(express.json());
+
+app.use(express.static("public"));
+
 // Start the server
 const PORT = 3340;
 app.listen(PORT, () => {
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,10 @@
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
-  res.send("Hello World");
+  res.render("index");
 });
 
-router.get("/:name/:roll", (req, res) => {
-  console.log(req.query); // Uncomment this line if you want to see the query parameters in the console
-  const { address } = req.query; // Fix the variable name here
-  const { name, roll } = req.params;
-  res.send(`Hello ${name}. You are from ${address} and your roll is ${roll}`);
+router.get("/componets", (req, res) => {
+  res.render("components");
 });
-
 module.exports = router;
